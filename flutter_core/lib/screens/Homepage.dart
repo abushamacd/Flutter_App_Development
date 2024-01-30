08d517819c932641d71c891e1dd401faa7926f1d
abushamacd <assiddik001@gmail.com>
2024-01-31 00:39:21 +0600

    single child scroll

--- flutter_core/lib/screens/Homepage.dart
+++ flutter_core/lib/screens/Homepage.dart
@@ -7,18 +7,77 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          height: 200,
-          width: 200,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: Colors.amber,
-          ),
-          child: Center(
-            child: Text(
-              "Hello World",
-              style: TextStyle(fontSize: 20),
-            ),
+        body:
+            // // container and text
+            //     Container(
+            //   height: 200,
+            //   width: 200,
+            //   decoration: BoxDecoration(
+            //     borderRadius: BorderRadius.circular(20),
+            //     color: Colors.amber,
+            //   ),
+            //   child: Center(
+            //     child: Text(
+            //       "Hello World",
+            //       style: TextStyle(fontSize: 20),
+            //     ),
+            //   ),
+            // ),
+
+            // // row and column
+            //     Column(
+            //   crossAxisAlignment: CrossAxisAlignment.start,
+            //   children: [
+            //     Text(
+            //       "Hello World",
+            //       style: TextStyle(fontSize: 20),
+            //     ),
+            //     Text(
+            //       "Welcome to earth",
+            //       style: TextStyle(fontSize: 20),
+            //     ),
+            //   ],
+            // ),
+
+            //   Row(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            // children: [
+            //   Text(
+            //     "Hello World",
+            //     style: TextStyle(fontSize: 20),
+            //   ),
+            //   Text(
+            //     "Welcome to earth",
+            //     style: TextStyle(fontSize: 20),
+            //   ),
+            // ],
+
+            // single child scroll
+            SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.amber,
+              ),
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.red,
+              ),
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.green,
+              ),
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.blue,
+              ),
+            ],
           ),
         ),
       ),
